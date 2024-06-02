7eb2adf17be865cb665bb0123414dc073593e72c
Jhosef <jhosefgrey@gmail.com>
2024-06-02 02:38:30 -0600

    falta las solicitudes

--- src/app/content/solicitud-agente/solicitud-agente.component.ts
+++ src/app/content/solicitud-agente/solicitud-agente.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AgenteVista } from '../agentes/models/agente';
+import { AgenteService } from '../agentes/services/agente.service';
+import { CommonModule, Location } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-solicitud-agente',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './solicitud-agente.component.html',
   styleUrl: './solicitud-agente.component.scss'
 })
-export class SolicitudAgenteComponent {
+export class SolicitudAgenteComponent implements OnInit {
+
+  agentes: AgenteVista[] = [];
+  idAgente: string = '';
+  constructor(private _agente: AgenteService, private _location: Location) { }
+
+  ngOnInit(): void {
+    this.getAllAgentes();
+  }
+
+  getAllAgentes() {
+    this._agente.getAll().subscribe((res) => {
+      this.agentes = res;
+    })
+  }
+
+  back() {
+    this._location.back();
+  }
+
+  loadData() {
+
+  }
 
 }
